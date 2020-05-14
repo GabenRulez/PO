79191ed1ad37f782e07149c6b4b0d85fa3f285c2
GabenRulez (PC Krosno) <wojtex1221@gmail.com>
2020-05-14 18:01:51 +0200

    init

--- lab4 (z Pawłem Kiełbasą)/lab4_pawel_after/lab4/src/pl/edu/agh/to/lab4/Application.java
+++ lab4 (z Pawłem Kiełbasą)/lab4_pawel_after/lab4/src/pl/edu/agh/to/lab4/Application.java
@@ -1,9 +1,36 @@
 package pl.edu.agh.to.lab4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        Finder suspects = new Finder(new PersonDatabase(), new PrisonersDatabase());
-        suspects.displayAllSuspectsWithName("Janusz");
+
+        PersonDatabase personDatabase = new PersonDatabase();
+        personDatabase.generateData();
+
+        PrisonersDatabase prisonersDatabase = new PrisonersDatabase();
+        prisonersDatabase.generateData();
+
+        StudentDatabase studentDatabase = new StudentDatabase();
+        studentDatabase.generateData();
+
+        List<SuspectAggregate> databases = new ArrayList<>();
+        databases.add(personDatabase);
+        databases.add(prisonersDatabase);
+        databases.add(studentDatabase);
+
+        Finder suspects = new Finder(new CompositeAggregate(databases));
+
+        List<SearchStrategy> strategies = new ArrayList<>();
+        strategies.add(new NameSearchStrategy("Janusz"));
+        strategies.add(new AgeSearchStrategy(77));
+
+
+
+
+
+
     }
 }
--- lab4 (z Pawłem Kiełbasą)/lab4_pawel_after/lab4/src/pl/edu/agh/to/lab4/StudentDatabase.java
+++ lab4 (z Pawłem Kiełbasą)/lab4_pawel_after/lab4/src/pl/edu/agh/to/lab4/StudentDatabase.java
@@ -2,6 +2,7 @@ package pl.edu.agh.to.lab4;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class StudentDatabase implements SuspectAggregate {
     public  final Collection<Student> students = new ArrayList<Student>();
@@ -11,10 +12,20 @@ public class StudentDatabase implements SuspectAggregate {
     public void addStudent(String firstName, String lastName, String index){
         students.add(new Student(firstName, lastName, index));
     }
+
     public void generateData(){
         addStudent("Stefan", "Dyta","768261");
         addStudent("yuq","yqkqyui","775684");
         addStudent("yatfyqt","uadjnd","738921");
-        addStudent();
+        addStudent("Jan", "Karzełek", "523643");
+    }
+
+    @Override
+    public Iterator<Suspect> iterator(SearchStrategy searchStrategy){
+        return new SuspectIterator(students.iterator(),searchStrategy);
+    }
+
+    public Collection<Student> getStudents() {
+        return students;
     }
 }
