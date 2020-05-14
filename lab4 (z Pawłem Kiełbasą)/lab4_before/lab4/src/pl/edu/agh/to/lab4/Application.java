ee25702a3060ed6945c115601900f37ab730b5d1
GabenRulez (PC Krosno) <wojtex1221@gmail.com>
2020-05-14 23:39:34 +0200

    Ukończono lab4.

--- lab4 (z Pawłem Kiełbasą)/lab4_before/lab4/src/pl/edu/agh/to/lab4/Application.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.to.lab4;
-
-public class Application {
-
-    public static void main(String[] args) {
-        Finder suspects = new Finder(new PersonDataProvider(), new PrisonersDatabase());
-        suspects.displayAllSuspectsWithName("Janusz");
-    }
-}
